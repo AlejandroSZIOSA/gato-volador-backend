bcb473716d6e6fcf507c426b21361c6f47784603
AlejandroSZIOSA <ggato33@gmail.com>
2025-05-06 15:42:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+//2
+const express = require("express");
+const app = express();
+//1
+app.listen(3000);
---
+++ app.ts
@@ -0,0 +1,8 @@
+//2
+import express from "express"; // Like in the frontend, cambia tsconfig.json
+
+/* import express = require("express"); */
+const app = express();
+
+//1
+app.listen(3000);
