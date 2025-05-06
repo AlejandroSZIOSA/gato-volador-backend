22db93d7b355e0addb6e28338217a3cbae9d9a7c
AlejandroSZIOSA <ggato33@gmail.com>
2025-05-06 17:21:33 +0200

    put method

--- routes/vinyls.ts
+++ routes/vinyls.ts
@@ -14,13 +14,28 @@ router.get("/", (req, res, next) => {
   res.status(200).json({ vinyls: vinyls });
 });
 
-//12
+//12 post method
 router.post("/vinyl", (req, res, next) => {
   const newVinyl: Vinyl = {
     id: new Date().toISOString(),
     artist: req.body.artist, //15
   };
   vinyls.push(newVinyl); //16
+  res.status(201).json({ message: "Vinyl added successfully", vinyls: vinyls });
+});
+
+//17 put method
+router.put("/vinyl/:vinylId", (req, res, next) => {
+  const tid = req.params.vinylId;
+  const vinylIndex = vinyls.findIndex((vinylItem) => vinylItem.id === tid);
+  if (vinylIndex <= 0) {
+    //updated vinyl information
+    vinyls[vinylIndex] = { id: vinyls[vinylIndex].id, artist: req.body.artist };
+    return res
+      .status(200)
+      .json({ message: "Vinyl updated successfully", vinyls: vinyls });
+  }
+  res.status(404).json({ message: "Vinyl not found" });
 });
 
 //4
