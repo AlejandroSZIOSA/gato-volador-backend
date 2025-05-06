f587dbd25da5de0fc2a5da4ee7695a7a4e553456
AlejandroSZIOSA <ggato33@gmail.com>
2025-05-06 17:26:21 +0200

    delete method

--- routes/vinyls.ts
+++ routes/vinyls.ts
@@ -6,7 +6,7 @@ import { Vinyl } from "../models/vinyl";
 //3
 const router = Router();
 //8
-const vinyls: Vinyl[] = []; //11 using the right type
+let vinyls: Vinyl[] = []; //11 using the right type
 
 //7
 router.get("/", (req, res, next) => {
@@ -36,6 +36,15 @@ router.put("/vinyl/:vinylId", (req, res, next) => {
       .json({ message: "Vinyl updated successfully", vinyls: vinyls });
   }
   res.status(404).json({ message: "Vinyl not found" });
+});
+
+//18 delete method
+
+router.delete("/vinyl/:vinylId", (req, res, next) => {
+  vinyls = vinyls.filter((vinylItem) => vinylItem.id !== req.params.vinylId);
+  res
+    .status(200)
+    .json({ message: "Vinyl deleted successfully", vinyls: vinyls });
 });
 
 //4
